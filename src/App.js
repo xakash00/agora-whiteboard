dd6d968d7346c8b8c5efb1ccdde29111012c60db
AkashS <akash.s@krishaweb.com>
2022-12-16 15:46:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { useEffect } from "react";
+import { createFastboard, createUI } from "@netless/fastboard";
 
 function App() {
+  const styles = {
+    width: "100%",
+    height: "72vh",
+    backgroundColor: "#fff",
+  };
+
+  let app;
+  async function mountFastboard(div) {
+    app = await createFastboard({
+      sdkConfig: {
+        appIdentifier: "yj-J8GWuEe2PfG3c9c975A/Qx3WgUu-5RCjDQ",
+        region: "in-mum",
+      },
+      joinRoom: {
+        uid: "104",
+        uuid: "415545207d0111eda6858354054d6a12",
+        roomToken:"NETLESSROOM_YWs9NU1hLUtTNU5Ob2s1TmRjQSZleHBpcmVBdD0xNjcxMTg5MTEzNzk3Jm5vbmNlPTE2NzExODU1MTM3OTcwMCZyb2xlPTEmc2lnPWY5Y2VmZmY5YTQwZTJjYjY3MTliMDM5OTkzYjJhOWMyYmJiZDRkZWZiNjQ2Yzk3Nzk0ZWUwZWI1NmE1ZTdmNmUmdXVpZD00MTU1NDUyMDdkMDExMWVkYTY4NTgzNTQwNTRkNmExMg",
+        isWritable: true 
+      },
+      managerConfig: {
+        cursor: false,
+      },
+    });
+    window.app = app;
+    return createUI(app, div);
+  }
+
+  useEffect(() => {
+    const timeout_id = setTimeout(
+      () => mountFastboard(document.getElementById("whiteboard")),
+      0
+    );
+    return () => clearTimeout(timeout_id);
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="whiteboard" style={styles}></div>
     </div>
   );
 }
